401503edf06e1fb9b2e82d2e71bfe18e1f147896
Madhavarapu Chandra Sekhara Sri Sai <madhavarapuchandrasekhara@gmail.com>
2025-08-05 15:18:53 +0530

    main.py

--- main.py
+++ main.py
@@ -317,10 +317,10 @@ def preview_download_pdf(file_bytes):
     try:
         base64_pdf = base64.b64encode(file_bytes).decode("utf-8")
         pdf_display = f"""
-        <iframe
-            src="data:application/pdf;base64,{base64_pdf}"
-            width="100%"
-            height="600px"
+        <iframe 
+            src="data:application/pdf;base64,{base64_pdf}" 
+            width="100%" 
+            height="600px" 
             type="application/pdf"
             style="border:none; box-shadow: 0 4px 8px rgba(0,0,0,0.1);"
         ></iframe>
